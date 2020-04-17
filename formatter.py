5deef32310531355271bc811ba81df9452cb027a
Takayamashi <ryu1208.takayama@gmail.com>
2020-04-17 12:23:18 +0900

    add translation to formatter.py

--- formatter.py
+++ formatter.py
@@ -1,12 +1,8 @@
 import os
 import sys
+from utils import translate_and_compose
 
-def main():
-    # Use like [python formatter.py "file_path"]
-    ROOT_PATH = str(sys.argv[1])
-    file_check(ROOT_PATH)
-
-def check(file_path, filename):
+def check(file_path):
     # 処理を記述
     f = open(file_path)
     data = f.read()  # ファイル終端まで全て読んだデータを返す
@@ -18,16 +14,21 @@ def check(file_path, filename):
         for i in range(len(lines)):
             if (i - 4) % 5 != 0 and i != 0:
                 output.append(lines[i])
-                output.append("\n")
+                if i != len(lines)-1:
+                    output.append("\n")
     else:
         for i in range(len(lines)):
             output.append(lines[i])
-            output.append("\n")
+            if i != len(lines)-1:
+                output.append("\n")
     
     f = open(file_path, "w")
     f.writelines(output)
     f.close()
 
+
+def translate(pathname, filename):
+    translate_and_compose(os.path.join(pathname,filename), os.path.join(pathname,"ja_" + filename), 'en', 'ja')
 
 
 def file_check(path):
@@ -35,8 +36,21 @@ def file_check(path):
         for filename in filenames:
             if filename.endswith('.srt'):
                 print("Checking : ", pathname, filename)
-                check(os.path.join(pathname,filename), filename)
+                check(os.path.join(pathname,filename))
+    
+    for pathname, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.endswith('.srt'):
+                print("Translating : ", pathname, filename)
+                translate(pathname, filename)
+
     print("finished")
+
+
+def main():
+    # Use like [python formatter.py "file_path"]
+    ROOT_PATH = str(sys.argv[1])
+    file_check(ROOT_PATH)
 
 
 if __name__ == "__main__":
